deb88dad2c71dd4520e0449375cd44ee614b01c3
carospahni <caro.spahni@protonmail.com>
2025-06-17 21:06:32 +0200

    Fix routing for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,12 @@ import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './App.css';
 
+// Handle initial load
+if (window.location.pathname !== '/' || !window.location.hash) {
+  const redirectTo = '/#' + window.location.pathname + window.location.search;
+  window.history.replaceState(null, '', redirectTo);
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <HashRouter>
     <App />
