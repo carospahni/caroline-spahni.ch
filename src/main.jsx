d34c1cd091f362a9e60a092699b31edbfe099136
carospahni <caro.spahni@protonmail.com>
2025-06-17 21:33:06 +0200

    fixes

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,6 @@ import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './App.css';
 
-<<<<<<< HEAD
-=======
-// Handle initial load
-if (window.location.pathname !== '/' || !window.location.hash) {
-  const redirectTo = '/#' + window.location.pathname + window.location.search;
-  window.history.replaceState(null, '', redirectTo);
-}
-
->>>>>>> 2cc40f501ed9b722dab6f7d272569619c807a9d8
 ReactDOM.createRoot(document.getElementById('root')).render(
   <HashRouter>
     <App />
